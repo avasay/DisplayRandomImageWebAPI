3ce3ee14e7fd0bd5599d27cd104a49eec273bf79
alejandrio.vasay <alejandrio.vasay@tccd.edu>
2021-10-01 09:39:04 -0500

    First commit

---
+++ Controllers/ImageController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.Extensions.Caching.Memory;
+
+namespace HttpGetRandomImageWebAPI.Controllers;
+
+[Route("[controller]/[action]")]
+public class ImageController : Controller
+{
+    IMemoryCache m_memoryCache;
+    public ImageController(IMemoryCache memoryCache)
+    {
+        m_memoryCache = memoryCache;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public ActionResult Random(string dirPath)
+    {
+        string msg = string.Empty;
+        if (dirPath != null)
+        {
+            try
+            {
+                string localPath = Path.Combine(Environment.CurrentDirectory, "Images", dirPath);
+                DirectoryCacher dirCacher = new DirectoryCacher(m_memoryCache, localPath);
+
+                List<string> listCache = dirCacher.GetListCache();
+
+                // Get a random filename from the cache
+                string pickedRandomImage = PickRandomFromCache(listCache);
+
+                // Instantiate the ImageCacher object
+                ImageCacher imageCacher = new ImageCacher(m_memoryCache, pickedRandomImage);
+
+                msg = imageCacher.GetImage() + " -- " + pickedRandomImage;
+            }
+            catch (Exception ex)
+            {
+                // Log something
+            }
+            return Content(msg);
+        }
+        else
+        {
+            return Content("Specify directory!");
+        }    
+    }
+    private string PickRandomFromCache(List<string> collection)
+    {
+        // Pick random file
+        Random R = new Random();
+        string imagePath = string.Empty;
+        int randomNumber = R.Next(0, collection.Count());
+        imagePath = collection.ElementAt(randomNumber);
+        return imagePath;
+    }
+
+}
---
+++ DirectoryCacher.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Caching.Memory;
+
+namespace HttpGetRandomImageWebAPI;
+public class DirectoryCacher
+{
+    IMemoryCache m_memoryCache;
+    private string m_path;
+
+    public DirectoryCacher(IMemoryCache memoryCache, string path)
+    {
+        m_memoryCache = memoryCache;
+
+        var dirInfo = new DirectoryInfo(path);
+        if (dirInfo.Exists)
+        {
+            m_path = path;
+        }
+        else
+        {
+            throw new DirectoryNotFoundException();
+        };
+
+    }
+
+    public List<string> GetListCache()
+    {
+        List<string> cacheList = new List<string>();
+        m_memoryCache.TryGetValue(m_path, out cacheList);
+
+        if (cacheList == null)
+        {
+            BuildCache(m_path);
+            m_memoryCache.TryGetValue(m_path, out cacheList);
+        }
+
+        return (List<string>)cacheList;
+    }
+
+    private void BuildCache(string path)
+    {
+        // Get a list of files from the given path
+        var extensions = new string[] { ".png", ".jpg", ".gif" };
+        var dirInfo = new DirectoryInfo(path);
+
+        List<System.IO.FileInfo> fileInfoList = dirInfo.GetFiles("*.*").Where(f => extensions.Contains(f.Extension.ToLower())).ToList();
+
+        //
+        // Check if directory is empty or not
+        //
+        if (fileInfoList.Count() != 0)
+        {
+            // Put all file names in our list
+            List<string> fileInfo2string = fileInfoList.Select(f => f.FullName.ToString()).ToList();
+
+            // Set cache options.
+            var memCacheEntryOptions = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromSeconds(120));
+
+            // Cache the list
+            m_memoryCache.Set(path, fileInfo2string, memCacheEntryOptions);
+
+        }
+        else
+        {
+            throw new FileNotFoundException();
+        };
+
+    }
+}
